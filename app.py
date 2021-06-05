16113cef3b1bc0f692a855d797c8079b83a5a112
Alessandra Julia <julialessandr@gmail.com>
2021-06-05 01:10:26 -0300

    Insert posts

--- app.py
+++ app.py
@@ -8,45 +8,51 @@ from sqlite3 import Error
 app = Flask(__name__)
 
 # 1. Cadastrar Posts
-@app.route('/novopost', methods=['GET','POST'])
-
+@app.route('/posts/cadastrar', methods=['GET','POST'])
 def cadastrar():
+
     if request.method == 'POST':
+
         titulo = request.form['titulo']
         descricao = request.form['descricao']
+
         mensagem = 'Erro - nao cadastrado'
+
         if titulo and descricao:
             registro = (titulo, descricao)
+
             try:
                 conn = sqlite3.connect('database/db-blog.db')
 
-                sql = ''' INSERT INTO posts (titulo, descricao)
+                sql = ''' INSERT INTO post(titulo, descricao)
                               VALUES(?,?) '''
 
                 cur = conn.cursor()
 
-                cur.execute(sql, [descricao])
+                cur.execute(sql, registro)
 
                 conn.commit()
 
-                mensagem = 'Cadastrado com sucesso'
+                mensagem = 'Sucesso - cadastrado'
 
             except Error as e:
                 print(e)
             finally:
                 conn.close()
+
     return render_template('cadastrar.html')
+
 
 # Listar topicos
 @app.route('/posts/listar', methods=['GET'])
 def listar():
     try:
         conn = sqlite3.connect('database/db-blog.db')
-        sql = '''SELECT * FROM posts'''
+        sql = '''SELECT * FROM post'''
         cur = conn.cursor()
         cur.execute(sql)
-        registros = cur.fetchall()
-        return render_template('listar.html', regs=registros)
+        posts = cur.fetchall()
+        return render_template('listar.html', regs=posts)
     except Error as e:
         print(e)
     finally:
